51fb641921d5719f09bfb8c8242d986b6b7c1391
Anusha Sonte Parameshwar <29819682+anushasonte@users.noreply.github.com>
2023-11-11 13:06:08 -0600

    Update task4.c

--- Lab3/task4.c
+++ Lab3/task4.c
@@ -8,13 +8,12 @@ unsigned char Y[200] = {
 int main()
 {
 for (int i = 0; i < 200; i++) {
- if (X[i] == Y[i]) {
+ if (X[i] != Y[i]) {
           printf("X[i] = %.2x, Y[i] = %.2x\n",X[i],Y[i]);
-          printf("Running benign code..\n");
+          printf("Running malicious code..\n");
           return 0;
    }
  } 
- printf("X[i] = %.2x, Y[i] = %.2x\n",X[i],Y[i]);
- printf("Running malicious code...\n");
+ printf("Running benign code...\n");
  return 0;
 }
